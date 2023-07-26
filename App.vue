8ce472d47d6dadc833491fe863dc9059126daabd
Lange591 <94658009+Lange591@users.noreply.github.com>
2023-07-26 15:33:25 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Pokedex />
+  </div>
+</template>
+
+<script>
+import Pokedex from './components/Pokedex'
+
+export default {
+  name: 'app',
+  components: {
+    Pokedex
+  }
+}
+</script>
+
+
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #424242;
+  margin-top: 60px;
+}
+</style>
